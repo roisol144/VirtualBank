696c2559a4cd016854c2808eb56cb4d7af728670
Roi Solomon <roisol144@gmail.com>
2024-10-10 09:14:08 +0300

    test git actions

--- auth_utils.py
+++ auth_utils.py
@@ -10,6 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # Keys
 ferney_key = os.environ.get("FERNET_KEY")
 fernet = Fernet(ferney_key.encode())
+TOKEN_EXPIRATION_HOURS = os.environ.get('TOKEN_EXPIRATION_HOURS')
 
 # logging config
 logging.basicConfig(level=logging.DEBUG, 
@@ -18,25 +19,23 @@ logging.basicConfig(level=logging.DEBUG,
 
 def generate_token(user_id):
     payload = {
-        'exp': datetime.datetime.now() + datetime.timedelta(hours=1),  # Token expiration time
+        'exp': datetime.datetime.now() + datetime.timedelta(hours = TOKEN_EXPIRATION_HOURS),  # Token expiration time
         'iat': datetime.datetime.now(),  # Issued at time
         'sub': user_id  # Subject of the token (user ID)
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-    logging.debug(f"Generated token for user{user_id}: {token}")
     return token
 
 def verify_token(token):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        logging.debug(f"Token verified. Payload{payload}")
-        logging.debug(f"for user{payload['sub']}")
+        logging.debug(f"Authentication token valid for user {payload['sub']}")
         return payload['sub']
     except jwt.ExpiredSignatureError:
-        logging.debug("Token Expired.")
+        logging.debug(f"Token Expired for user {payload['sub']}.")
         return None
     except jwt.InvalidTokenError:
-        logging.debug("Invalid token.")
+        logging.debug("Invalid token was supplied.")
         return None
     
     
